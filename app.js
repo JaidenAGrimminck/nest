5f88a84a3aac52b2b1c2142d098624636cf0ea18
jaidenagrimminck <jaidng1@gmail.com>
2024-11-29 14:48:14 +0100

    added preferences

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 
 const apiRouter = require('./api');
 
 const app = express();
-const port = 3333;
+let port;
 
 app.get('/', (req, res) => {
     // send public/index.html file
@@ -11,6 +12,11 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api', apiRouter);
+
+let data = fs.readFileSync('preferences.json');
+let preferences = JSON.parse(data);
+
+port = preferences.port;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
